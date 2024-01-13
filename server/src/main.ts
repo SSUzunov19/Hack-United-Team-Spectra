d17035552b7bfbf596331c00ec2c64a5defcf399
DNDonchev20 <85338879+DNDonchev20@users.noreply.github.com>
2024-01-13 14:40:13 +0200

    Change the port

--- server/src/main.ts
+++ server/src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(8080);
+  await app.listen(3000);
 
   if (module.hot) {
     module.hot.accept();
