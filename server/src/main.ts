12732ecd9bb2802c79452209e47ab254498d51e2
DNDonchev20 <85338879+DNDonchev20@users.noreply.github.com>
2024-01-13 14:25:04 +0200

    Update workflow file

--- server/src/main.ts
+++ server/src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(8080);
 
   if (module.hot) {
     module.hot.accept();
